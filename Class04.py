686ca9e071b7fce9002d8df3a4a198cf786e55c3
Rafael <rafael.florentino.barbosa@gmail.com>
2024-07-03 16:02:10 -0300

    Continuation Operators - class 04

---
+++ Class04.py
@@ -0,0 +1,23 @@
+# Division Integer
+
+n1 = 7
+n2 = 3
+
+division_integer = n1 // n2
+rest = n1 % n2
+expo = n1 ** n2
+
+print(f'Divisão inteira: {division_integer}')
+
+print(f'Resto da divisão: {rest}')
+
+print(f'Exponenciação: {expo}')
+
+#Assignment subtraction
+
+valor = 10
+valor -= 10
+
+#Assignment Multiplication
+
+valor *= 10
